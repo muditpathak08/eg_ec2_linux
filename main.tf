5d678ff0d71614f506ce22ed0473d0357fae1ac7
Mudit Pathak <mudit.pathak@coforge.com>
2023-11-28 12:28:52 +0530

    Remove unwanted lines

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 locals {
   volume_count   		     = var.ebs_volume_count
-  security_group_enabled = var.enabled && var.security_group_enabled
-  root_iops              = contains(["io1", "io2", "gp3"], var.root_volume_type) ? var.root_iops : null
-  ebs_iops               = contains(["io1", "io2", "gp3"], var.ebs_volume_type) ? var.ebs_iops : null
-  root_throughput        = var.root_volume_type == "gp3" ? var.root_throughput : null
-  ebs_throughput         = var.ebs_volume_type == "gp3" ? var.ebs_throughput : null
   root_volume_type       = var.root_volume_type
   reboot_actions_ok   =  ["arn:aws:sns:${var.region}:${var.ACCTID}:Ec2RebootRecover"]
   recover_actions_ok  =  ["arn:aws:sns:${var.region}:${var.ACCTID}:Ec2RebootRecover"]
@@ -49,14 +44,6 @@ data "aws_subnet" "test" {
 }
 
 
-# module "aws_security_group" {
-#   source      = "./modules/security_group"
-#   sg_count = length(var.security_groups)
-#   name = var.security_groups
-#   description = lookup(var.awsprops, "secgroupname")
-#   vpc_id      = var.vpc_id
-
-# } 
 
 module "new_security_group" {
   source = "./modules/security_group_new"
@@ -69,32 +56,6 @@ module "existing_sg_rules" {
   source = "./modules/existing_sg_rules"
   existing_sg_rules = var.existing_sg_rules
 }
-
-
-# resource "aws_security_group_rule" "ingress_rules" {
-#   count = length(var.ingress_rules)
-
-#   type              = "ingress"
-#   from_port         = var.ingress_rules[count.index].from_port
-#   to_port           = var.ingress_rules[count.index].to_port
-#   protocol          = var.ingress_rules[count.index].protocol
-#   cidr_blocks       = [var.ingress_rules[count.index].cidr_block]
-#   description       = var.ingress_rules[count.index].description
-#   security_group_id = module.aws_security_group.id[count.index]
-# }
-
-
-# resource "aws_security_group_rule" "egress_rules" {
-#   count = length(var.egress_rules)
-#   type              = "egress"
-#   from_port         = var.egress_rules[count.index].from_port
-#   to_port           = var.egress_rules[count.index].to_port
-#   protocol          = var.egress_rules[count.index].protocol
-#   cidr_blocks       = [var.egress_rules[count.index].cidr_block]
-#   description       = var.egress_rules[count.index].description
-#   security_group_id = module.aws_security_group.id[count.index]
-# }
-
 
 
 resource "aws_instance" "project-iac-ec2-linux" {
@@ -137,9 +98,7 @@ lifecycle {
 }
 
 resource "aws_eip_association" "eip_assoc" {
-  # count = contains(["Public","public","PUBLIC"], var.Subnet_Name) ? 0 : 1
   count       = strcontains(var.Subnet_Name, "Public") ? 1: 0
-  # count = strcontains("hello world", "wor") ? 1 : 0
   instance_id   = aws_instance.project-iac-ec2-linux.id
   allocation_id = var.eip_allocation_id
 }
