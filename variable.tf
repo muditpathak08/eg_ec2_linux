b1d412dd64b1a510d07f10450095abbb880850ac
Mudit Pathak <mudit.pathak@coforge.com>
2023-12-21 15:24:25 +0530

    Update

--- variable.tf
+++ variable.tf
@@ -419,6 +419,6 @@ variable "size" {
 #Size of the EBS needed to be created
 variable "instance_id" {
   description = "ID of the Instance created"
-  type        =  string
-  default     = ""
+  type        =  number
+  # default     = ""
 }
