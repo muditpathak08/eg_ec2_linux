15e9ef24640ca86bae446656645cc89b386ae1d5
Mudit Pathak <mudit.pathak@coforge.com>
2023-12-21 15:57:25 +0530

    Removed old module for SG

--- modules/security_group/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group" "security_groups" {
-  count = var.sg_count
-  name  = var.name[count.index]
-  description = var.description
-  vpc_id = var.vpc_id
-  // To Allow SSH Transport
-
-  // To Allow Port 80 Transport
-}
--- modules/security_group/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "id" {
-  value = aws_security_group.security_groups.*.id
-}    
-
-# output "security_group_id" {
-#   value = "${concat(aws_security_group.security_groups.*.id, tolist([""]))}"
-# }    
