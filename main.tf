8616bafc61bb7f82bdeb3ce2ec026c529805e7d3
Mudit Pathak <mudit.pathak@coforge.com>
2024-01-10 17:58:55 +0530

    Remve count

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ lifecycle {
 
 resource "aws_eip_association" "eip_assoc" {
   count       = strcontains(var.Subnet_Name, "public") ? 1: 0
-  instance_id   = aws_instance.project-iac-ec2-linux[0].id
+  instance_id   = aws_instance.project-iac-ec2-linux.id
   allocation_id = var.eip_allocation_id
 }
 
@@ -118,7 +118,7 @@ module "ebs_volume" {
     ebs_device_name = var.ebs_device_name
     snapshot_id       = var.snapshot_id  ## To be set if Volume to be created from Snapshot
     efs_tags = var.efs_tags
-    instance_id = aws_instance.project-iac-ec2-linux[0].id
+    instance_id = aws_instance.project-iac-ec2-linux.id
     # ... omitted
   }
 
@@ -140,7 +140,7 @@ resource "aws_cloudwatch_metric_alarm" "reboot-alarm" {
   alarm_actions             = var.reboot_actions_alarm
   ok_actions                = local.reboot_actions_ok
     dimensions = {
-        InstanceId = aws_instance.project-iac-ec2-linux[0].id
+        InstanceId = aws_instance.project-iac-ec2-linux.id
       }
   }
 resource "aws_cloudwatch_metric_alarm" "recover-alarm" {
@@ -158,6 +158,6 @@ resource "aws_cloudwatch_metric_alarm" "recover-alarm" {
   alarm_actions             = var.recover_actions_alarm
   ok_actions                = local.recover_actions_ok
     dimensions = {
-        InstanceId = aws_instance.project-iac-ec2-linux[0].id
+        InstanceId = aws_instance.project-iac-ec2-linux.id
       }
   }
